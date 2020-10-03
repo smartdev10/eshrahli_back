e5ab0596dac8a0bc61e001dc26ec6164b7cffcd6
Abdeljalil Ait etaleb <abdeljalil.aitetaleb@gmail.com>
2020-10-03 23:53:03 +0100

    updated

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -18,6 +18,7 @@ export class StudentService {
 
     async findStudentRequests(student: number) {
         return await this.requestsRepository.find({
+            take:30,
             where:{
                 student
             },
