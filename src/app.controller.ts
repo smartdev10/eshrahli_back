dbc45220f825393e105ac1a9a16402093ea4aa8a
Abdeljalil Ait etaleb <abdeljalil.aitetaleb@gmail.com>
2020-10-04 15:57:36 +0100

    updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/home')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/payement')
+  @Render('index')
+  payement() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/redirect')
+  @Render('redirect')
+  redirect() {
+    return { message: 'Hello world!' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname,'../images'))
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(7000);
 }
