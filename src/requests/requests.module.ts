c4bfe424690ee763c0fa68ccd32dfd7b9a617724
Abdeljalil Ait etaleb <abdeljalil.aitetaleb@gmail.com>
2020-10-23 12:40:51 +0100

    updated

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestService } from './requests.service';
 import { RequestController } from './requests.controller';
 import { SRequest } from 'src/entities/requests.entity';
-import { ONESIGNAL_MODULE_OPTIONS , TEACHER_ONSIGNAL } from 'src/onesignal/interface/onesignal.config';
+import { ONESIGNAL_MODULE_OPTIONS } from 'src/onesignal/interface/onesignal.config';
 import { OneSignalService } from 'src/onesignal/onesignal.service';
 import { TeacherService } from 'src/teachers/teachers.service';
 import { Teacher } from 'src/entities/teachers.entity';
@@ -36,7 +36,6 @@ const clientTeacherFactory = {
         TypeOrmModule.forFeature([Level]), 
         TypeOrmModule.forFeature([Subject]),
         OneSignalModule.register({
-            name:TEACHER_ONSIGNAL,
             appId:process.env.TEACHER_APP_ID,
             restApiKey:process.env.TEACHER_REST_API_KEY,
         }),
